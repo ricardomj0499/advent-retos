76976ee11c9883ffea1b1a064b4d86a3fdfafbda
ricardomj0499 <ricardomj0499@hotmail.com>
2022-11-04 21:25:55 -0600

    reto 12 completado

--- retos/reto12.js
+++ retos/reto12.js
@@ -6,60 +6,28 @@
  Necesitamos una función que nos diga la longitud mínima del salto del trineo para ir evitando todos los obstáculos en la ruta.
 */
 
-function getModulus(len, modulo) {
-    let indices = [];
-
-    for (let index = 0; index < len; index++) {
-        if (index % modulo === 0) {
-            indices.push(index);
-        }
-    }
-
-    return indices;
-}
-
 export default function getMinJump(obstacles) {
     const numObstacles = obstacles.length;
     if (!numObstacles) {
         return 1;
     }
-    const modToTest = 5;
-    for (let index = 2; index < +Math.max(...obstacles); index++) {
-        const element = array[index];
+
+    const modToTest = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    for (const mod in modToTest) {
+        let isMin = true;
+        console.log("mod: ", modToTest[mod]);
+        for (const obs in obstacles) {
+            if (obstacles[obs] % modToTest[mod] === 0) {
+                console.log("if mod === 0");
+                isMin = false;
+                break;
+            }
+            console.log("obs: ", obstacles[obs]);
+        }
+        if (isMin) {
+            return modToTest[mod];
+        }
     }
-
-    return true;
+    return -1;
 }
-console.log(getModulus(15, 3));
-
-const obstacles = [5, 3, 6, 7, 9];
-getMinJump(obstacles); // -> 4
-
-// S es salto, X es obstáculo
-/* Así quedaría la representación:
-0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-.  .  .  X  .  X  X  X  .  X  . 
-S-----------S-----------S-------
-*/
-
-const obstacles1 = [2, 4, 6, 8, 10];
-getMinJump(obstacles1); // -> 7
-
-/* Así quedaría la representación:
-0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-.  .  X  .  X  .  X  .  X  .  X 
-S--------------------S---------
-
-// Longitudes de salto:
-// 1 caería en el 2
-// 2 caería en el 2
-// 3 caería en el 6
-// 4 caería en el 4
-// 5 caería en el 10
-// 6 caería en el 6
-// 7 es el ideal!!! ✅
-
-getMinJump([1, 2, 3, 5]) // -> 4
-getMinJump([3, 7, 5]) // -> 2
-getMinJump([9, 5, 1]) // -> 2
-*/
